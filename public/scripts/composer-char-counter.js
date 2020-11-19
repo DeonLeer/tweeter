680641bd0397afa5e68095ebdcd287d4209f15d1
deonleer <deon.leer96@gmail.com>
2020-11-19 01:30:13 +0000

    added functional character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function() {
+  
+  const $textArea = $('textarea');
+  
+
+  $textArea.on('input', function() {
+    
+    let charCount = 140;
+
+    charCount = 140 - ($(this).val().length);
+    
+
+    $(this).parent().children().last().children().last().val(charCount)
+
+
+  })
+
+})
+
+// console.log($(this).parent().children().last().children().last())
