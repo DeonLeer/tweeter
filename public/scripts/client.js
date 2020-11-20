75e9c1f3d7f77ffae6a58151ff00b33677108f6d
deonleer <deon.leer96@gmail.com>
2020-11-20 07:12:18 +0000

    displays tweets at top of page after submitted

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,29 +1,6 @@
 $(document).ready(function() {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+
 
   const createTweetElement = function (tweet) { 
     let $tweet = 
@@ -45,25 +22,39 @@ $(document).ready(function() {
 
   const renderTweets = function(tweets) {
     for (let tweet of tweets) {
+      
       let $tweet = createTweetElement(tweet)
-      $('#tweets').append($tweet); 
+      $('#tweets').prepend($tweet); 
     }
   }
 
+  const loadTweets = function() {
+    $(function() {
+      $.get("/tweets", function(data) {
+        renderTweets(data);
+      })
+    })
+  }
 
-  renderTweets(data);
+  loadTweets()
+
+  $(function() {
+    $("form").on( "submit", function( event ) {
+      event.preventDefault();
+      const $tweet = $(this).serialize();
+      $.post("/tweets", $tweet)
+      .then(function () {
+        $.get("/tweets", function(data) {
+          const newTweet = data[data.length - 1];
+          const $newTweet = createTweetElement(newTweet);
+          $('#tweets').prepend($newTweet);
+        })
+      })
+    });
+  });
+  
 
 })
 
 
-
-
-
-$(function() {
-  $("form").on( "submit", function( event ) {
-    event.preventDefault();
-    const $tweet = $(this).serialize();
-    $.post("/tweets", $tweet)
-  });
-});
 
